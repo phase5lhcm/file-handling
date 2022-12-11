0ae600632c962ad29c91eeb7f0c9f2fc6dd2bce3
Christine Maynard <christine.ri.maynard@gmail.com>
2022-12-10 19:15:15 -0500

    implement a stack that can push and pop an element

--- src/main/java/ImplementStack.java
+++ src/main/java/ImplementStack.java
@@ -2,7 +2,8 @@
 
 /**
  * Implement a linear sata structure with the following features:
- * FIFO
+ * FIFO retrieval of nodes
+ * push an element onto the stack and remove an element from the stack
  */
 class Stacks {
     static final int MAX = 100;
@@ -23,10 +24,29 @@ class Stacks {
         } else {
             // increment index of topEle by 1 to become 0
             arr[++topEle] = x;
-            System.out.println("Successfuly add"+x+"into your stack");
+            System.out.println("Successfuly add "+x+" into your stack");
             return true;
         }
      }
+     // remove an element from the stack
+    int pop(){
+        // stack is empty, there isn't anything to remove
+        if(topEle<0){
+            System.out.println("Stack underflow");
+            return 0;
+        }else{
+            int x = arr[topEle--];
+            return x;
+        }
+    }
+
 }
 public class ImplementStack {
+    public static void main(String[] args){
+        Stacks s = new Stacks();
+        for( int i = 0; i < 5; i++){
+            s.push(10+i);
+        }
+        System.out.println(s.pop() + " was popped from stack"); // removes last element added to the stack :)
+    }
 }
